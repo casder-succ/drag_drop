05570964c2b94b65fba721b15c08f6d7f61b4990
casder <casderiopus@gmail.com>
2022-04-30 23:42:42 +0300

    feat: add logic fot fetching data

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import styled from 'styled-components';
 import Lane from '../components/Lane/Lane';
 
@@ -14,22 +14,52 @@ const BoardWrapper = styled.div`
 `;
 
 class Board extends Component {
-  render() {
-    const lanes = [
-      { id: 1, title: 'To Do' },
-      { id: 2, title: 'In Progress' },
-      { id: 3, title: 'Review' },
-      { id: 4, title: 'Done' },
-    ];
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+            loading: false,
+            error: '',
+        };
+    }
 
-    return (
-      <BoardWrapper>
-        {lanes.map(lane => (
-          <Lane key={lane.id} title={lane.title} />
-        ))}
-      </BoardWrapper>
-    );
-  }
+    async componentDidMount() {
+        try {
+            this.setState({loading: true});
+
+            const data = await fetch('assets/data.json');
+            const dataJSON = await data.json();
+
+            if (dataJSON) {
+                this.setState({
+                    loading: false,
+                    data: dataJSON,
+                });
+            }
+        } catch (e) {
+            this.setState({error: e.message, loading: false});
+        }
+    }
+
+    render() {
+        const lanes = [
+            {id: 1, title: 'To Do'},
+            {id: 2, title: 'In Progress'},
+            {id: 3, title: 'Review'},
+            {id: 4, title: 'Done'},
+        ];
+
+        return (
+            <BoardWrapper>
+                {lanes.map(lane => (
+                    <Lane key={lane.id} title={lane.title}
+                          tickets={this.state.data.filter(item => item.lane === lane.id)}
+                          loading={this.state.loading}
+                          error={this.state.error}/>
+                ))}
+            </BoardWrapper>
+        );
+    }
 }
 
 export default Board;
