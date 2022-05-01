58971a0a4ffa52aa86e3b1bcf6a7439410e22192
casder <casderiopus@gmail.com>
2022-05-01 11:17:50 +0300

    feat: implement HOC component for data fetching

---
+++ src/WithDataFetch.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+export default function WithDataFetch(Component) {
+    return class extends React.Component {
+        constructor(props) {
+            super(props);
+
+            this.state = {
+                data: [],
+                loading: false,
+                error: '',
+            };
+        }
+
+        async componentDidMount() {
+            try {
+                this.setState({loading: true});
+
+                const data = await fetch(this.props.dataSource);
+                const dataJSON = await data.json();
+
+                if (dataJSON) {
+                    this.setState({
+                        loading: false,
+                        data: dataJSON,
+                    });
+                }
+            } catch (e) {
+                this.setState({error: e.message, loading: false});
+            }
+        }
+
+        render() {
+            const {data, loading, error} = this.state;
+
+            return (
+                <Component
+                    data={data}
+                    loading={loading}
+                    error={error}
+                    {...this.props}
+                />
+            )
+        }
+    }
+}
