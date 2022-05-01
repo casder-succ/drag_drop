98ee78192a30cc2baa4722f9c850ae5d10b1fae1
casder <casderiopus@gmail.com>
2022-05-01 12:09:11 +0300

    feat: integrate HOC for data fetching to board

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
+import React, {Component} from 'react';
+import styled, {createGlobalStyle} from 'styled-components';
 import Board from './Board';
 import Header from '../components/Header/Header';
 
@@ -8,8 +8,8 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-      sans-serif;
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
@@ -20,17 +20,24 @@ const AppWrapper = styled.div`
 `;
 
 class App extends Component {
-  render() {
-    return (
-      <>
-        <GlobalStyle />
-        <AppWrapper>
-          <Header />
-          <Board />
-        </AppWrapper>
-      </>
-    );
-  }
+    render() {
+        const lanes = [
+            {id: 1, title: 'To Do'},
+            {id: 2, title: 'In Progress'},
+            {id: 3, title: 'Review'},
+            {id: 4, title: 'Done'},
+        ];
+        
+        return (
+            <>
+                <GlobalStyle/>
+                <AppWrapper>
+                    <Header/>
+                    <Board lanes={lanes} dataSource={'assets/data.json'}/>
+                </AppWrapper>
+            </>
+        );
+    }
 }
 
 export default App;
--- src/containers/Board.js
+++ src/containers/Board.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
 import Lane from '../components/Lane/Lane';
+import WithDataFetch from "../WithDataFetch";
 
 const BoardWrapper = styled.div`
   display: flex;
@@ -13,53 +14,18 @@ const BoardWrapper = styled.div`
   }
 `;
 
-class Board extends Component {
-    constructor() {
-        super();
-        this.state = {
-            data: [],
-            loading: false,
-            error: '',
-        };
-    }
 
-    async componentDidMount() {
-        try {
-            this.setState({loading: true});
-
-            const data = await fetch('assets/data.json');
-            const dataJSON = await data.json();
-
-            if (dataJSON) {
-                this.setState({
-                    loading: false,
-                    data: dataJSON,
-                });
-            }
-        } catch (e) {
-            this.setState({error: e.message, loading: false});
-        }
-    }
-
-    render() {
-        const lanes = [
-            {id: 1, title: 'To Do'},
-            {id: 2, title: 'In Progress'},
-            {id: 3, title: 'Review'},
-            {id: 4, title: 'Done'},
-        ];
-
-        return (
-            <BoardWrapper>
-                {lanes.map(lane => (
-                    <Lane key={lane.id} title={lane.title}
-                          tickets={this.state.data.filter(item => item.lane === lane.id)}
-                          loading={this.state.loading}
-                          error={this.state.error}/>
-                ))}
-            </BoardWrapper>
-        );
-    }
+const Board = ({lanes, data, error, loading}) => {
+    return (
+        <BoardWrapper>
+            {lanes.map(lane => (
+                <Lane key={lane.id} title={lane.title}
+                      tickets={data.filter(item => item.lane === lane.id)}
+                      loading={loading}
+                      error={error}/>
+            ))}
+        </BoardWrapper>
+    );
 }
 
-export default Board;
+export default WithDataFetch(Board);
